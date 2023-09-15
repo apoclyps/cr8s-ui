7f87574ce439152ad8ecce1c98dbbcc890c96ff1
apoclyps <kyle90adam@hotmail.com>
2023-09-15 17:24:26 +0100

    improving the welcome page

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -5,18 +5,23 @@ use crate::{contexts::CurrentUserContext, Route};
 
 #[function_component(Home)]
 pub fn home() -> Html {
-    let current_user_ctx = use_context::<CurrentUserContext>().expect("Current user context is missing");
+    let current_user_ctx =
+        use_context::<CurrentUserContext>().expect("Current user context is missing");
 
     match &current_user_ctx.user {
         Some(user) => {
             html! {
                 <div class="container">
-                    {"Welcome user "}{user.username.clone()}
+                    <p class="text-center">
+                        {"Welcome user "}{user.username.clone()}<br/>
+                        <small>{"with ID "}{user.id.clone()}</small><br/>
+                        <small>{"created at "}{user.created_at.clone()}</small>
+                    </p>
                 </div>
             }
-        },
+        }
         None => html! {
             <Redirect<Route> to={Route::Login} />
-        }
+        },
     }
 }
